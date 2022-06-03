ae8f8611121c0ed435593eec87b3e66f92935ed2
Tmahn001 <toluiwalewa@gmail.com>
2022-06-03 12:01:00 +0100

    1st commit

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+//Database params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'test-scandiweb');
+
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+//URLROOT (Dynamic links)
+define('URL_ROOT', 'http://localhost/test-scandiweb');
+
+//Sitename
+define('SITENAME', 'MVC Framework!!!');
